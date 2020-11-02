c9fc827a927a2a04686b006760f715958c1c6337
hoang anh nguyen <nghoanganh19@gmail.com>
2020-11-02 12:44:11 +0700

    add error mdw

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const calculationController = require('./controllers/calculation');
 const logger = require('morgan');
+var createError = require('http-errors');
+const { allowedNodeEnvironmentFlags } = require("process");
 
 const app = express();
 // ""
@@ -36,17 +38,17 @@ app.use('/', calculationController);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
-  });
+});
   
   // error handler
-  app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+    res.locals.errStatus = err.status || 500;
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
-  });
+    res.render('error',  {title: 'BTCN03 - ERROR PAGE'});
+});
   
 module.exports = app;
--- controllers/calculation.js
+++ controllers/calculation.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const NUM_TO_ROUND = 100000000;
 
 router.get('/', (req, res) => {
-    res.render('calculation');
+    res.render('calculation', {title: 'BTCN03 - Calculation'});
 });
 
 router.post('/', (req, res, next) => {
@@ -11,34 +11,38 @@ router.post('/', (req, res, next) => {
     let secondNum = parseFloat(req.body.sothuhai);
     let operator = req.body.operator;
     let result;
+    let title = 'BTCN03 - Calculation';
     switch (operator) {
         case 'plus': {
             result = firstNum + secondNum;
             let isPlus = true;
-            res.render('calculation', {firstNum, secondNum, result, isPlus});
+            res.render('calculation', {title, firstNum, secondNum, result, isPlus});
             break;
         };
         case 'minus': {
             result = firstNum - secondNum;
             let isMinus = true;
-            res.render('calculation', {firstNum, secondNum, result, isMinus});
+            res.render('calculation', {title, firstNum, secondNum, result, isMinus});
             break;
         };
         case 'multi': {
             result = firstNum * secondNum;
             let isMulti = true;
-            res.render('calculation', {firstNum, secondNum, result, isMulti});
+            res.render('calculation', {title, firstNum, secondNum, result, isMulti});
             break;
         };
         case 'devide': {
             result = firstNum / secondNum;
             let isDevide = true;
-            res.render('calculation', {firstNum, secondNum, result, isDevide});
+            res.render('calculation', {title, firstNum, secondNum, result, isDevide});
             break;
         };
         default:
             break;
     }
+});
+router.get('/error', (req, res, next) => {
+    next();
 });
 
 module.exports = router;
