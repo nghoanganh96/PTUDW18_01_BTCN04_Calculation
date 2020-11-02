8c78ace782c6ccbe89cc1ff6a42386c36cb6b666
hoang anh nguyen <nghoanganh19@gmail.com>
2020-11-02 11:54:09 +0700

    add www file

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const calculationController = require('./controllers/calculation');
 const logger = require('morgan');
 
 const app = express();
-const port = process.env.PORT || 3000;
 // ""
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -34,6 +33,20 @@ app.set('view engine', 'hbs'); // set view engine of express is that engine abov
 // routers
 app.use('/', calculationController);
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+module.exports = app;
